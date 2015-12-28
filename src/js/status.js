0b9a7834a85cb0ac3201c5c233cec0f20aeb3acf
Adriano Interaminense <adriano.interaminense@gmail.com>
2015-12-28 00:19:44 -0200

    news settings

--- src/js/status.js
+++ src/js/status.js
@@ -1,41 +1,47 @@
-//estrelas
+function init(){
 
-var style = ["style1", "style2", "style3", "style4"];
-var tam = ["tam1", "tam1", "tam1", "tam2", "tam3"];
-var opacity = ["opacity1", "opacity1", "opacity1", "opacity2", "opacity2", "opacity3"];
+  //estrelas
 
-function getRandomArbitrary(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+  var style = ["style1", "style2", "style3", "style4"];
+  var tam = ["tam1", "tam1", "tam1", "tam2", "tam3"];
+  var opacity = ["opacity1", "opacity1", "opacity1", "opacity2", "opacity2", "opacity3"];
 
-var estrela = "";
-var qtdeEstrelas = 500;
-var noite = document.querySelector(".constelacao");
-var widthWindow = window.innerWidth;
-var heightWindow = window.innerHeight;
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-for (var i = 0; i < qtdeEstrelas; i++) {
-  estrela += "<span class='estrela " + style[getRandomArbitrary(0, 4)] + " " + opacity[getRandomArbitrary(0, 6)] + " "
-  + tam[getRandomArbitrary(0, 5)] + "' style='animation-delay: ." +getRandomArbitrary(0, 9)+ "s; left: "
-  + getRandomArbitrary(0, widthWindow) + "px; top: " + getRandomArbitrary(0, heightWindow) + "px;'></span>";
-}
+  var estrela = "";
+  var qtdeEstrelas = 500;
+  var noite = document.querySelector(".constelacao");
+  var widthWindow = window.innerWidth;
+  var heightWindow = window.innerHeight;
 
-noite.innerHTML = estrela;
+  for (var i = 0; i < qtdeEstrelas; i++) {
+    estrela += "<span class='estrela " + style[getRandomArbitrary(0, 4)] + " " + opacity[getRandomArbitrary(0, 6)] + " "
+    + tam[getRandomArbitrary(0, 5)] + "' style='animation-delay: ." +getRandomArbitrary(0, 9)+ "s; left: "
+    + getRandomArbitrary(0, widthWindow) + "px; top: " + getRandomArbitrary(0, heightWindow) + "px;'></span>";
+  }
 
-//meteoros
+  noite.innerHTML = estrela;
 
-var numeroAleatorio = 5000;
+  //meteoros
 
-setTimeout(function(){
-  carregarMeteoro();
-}, numeroAleatorio);
+  var numeroAleatorio = 5000;
 
-function carregarMeteoro(){
-  setTimeout(carregarMeteoro, numeroAleatorio);
-  numeroAleatorio = getRandomArbitrary(5000, 10000);
-  var meteoro = "<div class='meteoro "+ style[getRandomArbitrary(0, 4)] +"'></div>";
-  document.getElementsByClassName('chuvaMeteoro')[0].innerHTML = meteoro;
   setTimeout(function(){
-    document.getElementsByClassName('chuvaMeteoro')[0].innerHTML = "";
-  }, 1000);
+    carregarMeteoro();
+  }, numeroAleatorio);
+
+  function carregarMeteoro(){
+    setTimeout(carregarMeteoro, numeroAleatorio);
+    numeroAleatorio = getRandomArbitrary(5000, 10000);
+    var meteoro = "<div class='meteoro "+ style[getRandomArbitrary(0, 4)] +"'></div>";
+    document.getElementsByClassName('chuvaMeteoro')[0].innerHTML = meteoro;
+    setTimeout(function(){
+      document.getElementsByClassName('chuvaMeteoro')[0].innerHTML = "";
+    }, 1000);
+  }
+
 }
+
+window.onload = init;
